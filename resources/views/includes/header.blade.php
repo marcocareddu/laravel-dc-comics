58f4cc2f23d56796bb3e4bc576d17b9a7d1aecb8
marcocareddu <saldumaccu@tuta.io>
2023-08-27 16:40:04 +0200

    Header  - Fixed name path

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,7 +6,7 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav d-flex w-100 justify-content-between">
                         <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('comics.list') }}">Comics</a>
+                            <a class="nav-link active" href="{{ route('comics.index') }}">Comics</a>
                         </li>
                         <li class="nav-item">
                             <a class="btn btn-success" href="{{ route('comics.create') }}">+</a>
