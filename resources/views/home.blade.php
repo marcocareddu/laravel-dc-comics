c3db96a0e1ca0cf09b026b81508b4281a283705b
marcocareddu <saldumaccu@tuta.io>
2023-08-27 14:50:59 +0200

    HOME - Added base card template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,27 @@
 @extends('layouts.main')
 
 @section('title', 'Home')
+
+@section('main')
+
+    <main>
+        <div class="container">
+            <div class="row">
+
+                {{-- Dynamic image HERE --}}
+                @for ($i = 0; $i < 10; $i++)
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Description</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+
+        </div>
+    </main>
+
+@endsection
