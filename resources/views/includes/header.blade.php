e0c16d382d0a59dde7858b0d1aa56d88d5148607
marcocareddu <saldumaccu@tuta.io>
2023-08-27 14:45:20 +0200

    Header - Added base template

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,16 @@
+<header class="bg-light">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Home</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Comics</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</header>
