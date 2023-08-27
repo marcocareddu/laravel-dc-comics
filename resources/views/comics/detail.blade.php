bfc70a2982ee439e96b0fe9c6a43ed9baa39516e
marcocareddu <saldumaccu@tuta.io>
2023-08-27 16:27:44 +0200

    Added details & list page

---
+++ resources/views/comics/detail.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main')
+
+    <main>
+        <div class="container">
+            <div class="row">
+
+                {{-- Dynamic data HERE --}}
+                <div class="card col-6 m-3" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Title</h5>
+                        <p class="card-text">Description</p>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
---
+++ resources/views/comics/list.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main')
+
+    <main>
+        <div class="container">
+            <div class="row">
+
+                {{-- Dynamic image HERE --}}
+
+                {{-- !! TODO USE FOREACH INSTEAD FOR --}}
+                @for ($i = 0; $i < 10; $i++)
+                    <div class="card col-6 m-3" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="Image">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Description</p>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                @endfor
+
+            </div>
+        </div>
+    </main>
+
+@endsection
