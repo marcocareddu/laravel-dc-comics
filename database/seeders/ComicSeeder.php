0c9e2add5704e28627f7aa5b39c7947b852e0abb
marcocareddu <saldumaccu@tuta.io>
2023-08-27 18:56:43 +0200

    Added comic seeder

---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->fill($comic);
+            $new_comic->artists = implode(",", $comic['artists']);
+            $new_comic->writers = implode(",", $comic['writers']);
+            $new_comic->save();
+        }
+    }
+}
