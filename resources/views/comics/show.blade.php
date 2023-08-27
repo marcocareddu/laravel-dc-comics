b3cb8743223a22548444bdd67c92afdb44fc05a1
marcocareddu <saldumaccu@tuta.io>
2023-08-27 20:00:14 +0200

    Fixed Style

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,15 +6,54 @@
 
     <main>
         <div class="container">
-            <div class="row">
+            <div class="d-flex justify-content-center row">
 
                 {{-- Dynamic data HERE --}}
-                <div class="card col-6 m-3" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="Image">
+                <div class="card m-3 col-6">
+                    <img src="{{ $comic->thumb }}" class="card-img-top img-fluid" alt="Image">
                     <div class="card-body">
                         <h5 class="card-title">{{ $comic->title }}</h5>
                         <p class="card-text">{{ $comic->description }}</p>
-                        <a href="#" class="btn btn-primary">{{ $comic->title }}</a>
+
+                        {{-- Writers & Artists table --}}
+                        <div class="d-flex justify-content-between">
+                            <table>
+                                <tr>
+                                    <th colspan="2" align="left">Talent</th>
+                                </tr>
+                                <tr>
+                                    <td class="title">Art by:</td>
+                                    <td>
+                                        <a href="#"> {{ $comic->artists }} </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="title">Written by:</td>
+                                    <td>
+                                        <a href="#"> {{ $comic->writers }} </a>
+                                    </td>
+                                </tr>
+                            </table>
+
+                            {{-- Info Table --}}
+                            <table>
+                                <tr>
+                                    <th colspan="2" align="left">Specs</th>
+                                </tr>
+                                <tr>
+                                    <td class="title">Series:</td>
+                                    <td>{{ $comic->series }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="title">U.S. Price:</td>
+                                    <td>{{ $comic->price }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="title">On Sale Date:</td>
+                                    <td>{{ $comic->sale_date }}</td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
