ce81d748037637727eba9a16f6f6ff135b2c6593
marcocareddu <saldumaccu@tuta.io>
2023-08-27 20:22:25 +0200

    Add some style to create page

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.main')
 
-@section('title', 'Comic')
+@section('title', 'Add')
 
 @section('main')
 
     <main>
         <div class="container text-center">
-
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <div class="row">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
             </div>
         </div>
     </main>
