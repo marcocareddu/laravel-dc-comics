2513c6dfb1a9bf5253ec8808a6d7d9fd4dc1b645
marcocareddu <saldumaccu@tuta.io>
2023-08-29 14:48:36 +0200

    Index - Fixed route button

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
 
                 {{-- Dynamic image HERE --}}
-                @foreach ($comics as $index => $comic)
+                @foreach ($comics as $comic)
                     <div class="card col-6 m-3" style="width: 18rem;">
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="Image">
                         <div class="card-body">
@@ -19,7 +19,7 @@
                             <div class="d-flex justify-content-between">
 
                                 {{-- Detail Button --}}
-                                <a href="{{ url("comics/$index") }}" class="btn btn-primary">Details</a>
+                                <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary">Details</a>
 
                                 {{-- Delete Button --}}
                                 <form action="{{ route('comics.destroy', $comic) }}" method="POST">
