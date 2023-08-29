cff5458cfb17418a574172b65e6a30c8beb972d9
marcocareddu <saldumaccu@tuta.io>
2023-08-29 16:22:37 +0200

    Added trash buttons

--- resources/views/comics/trash.blade.php
+++ resources/views/comics/trash.blade.php
@@ -9,24 +9,42 @@
             <h1 class="text-center">Trashed</h1>
             <div class="row">
 
-                {{-- Dynamic image HERE --}}
-                @foreach ($comics as $comic)
+                @forelse ($comics as $comic)
+                    {{-- Dynamic image HERE --}}
                     <div class="card col-6 m-3" style="width: 18rem;">
                         <img src="{{ $comic->thumb }}" class="card-img-top" alt="Image">
                         <div class="card-body">
                             <h5 class="card-title">{{ $comic->title }}</h5>
 
                             {{-- Buttons --}}
-                            <div class="d-flex justify-content-between">
+                            <div class="d-flex justify-content-evenly">
 
-                                {{-- All Comics Button --}}
-                                <a href="{{ route('comics.index', $comic) }}" class="btn btn-primary">All Comics</a>
+                                {{-- Delete Button --}}
+                                <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Delete</button>
+                                </form>
+
+                                {{-- Restore Button --}}
+                                <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-success">Restore</button>
+                                </form>
 
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @empty
+                    <div class="alert alert-success">There's no elements</div>
+                @endforelse
+                <div class="d-flex justify-content-center">
 
+                    {{-- All Comics Button --}}
+                    <a href="{{ route('comics.index', $comic) }}" class="btn btn-primary">Return to Comics</a>
+
+                </div>
             </div>
         </div>
     </main>
